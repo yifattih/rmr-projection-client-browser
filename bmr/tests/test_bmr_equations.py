f690f75b62ee8d3660e97098eec10e116c30d364
yifattih <yifattih@proton.me>
2024-10-12 05:38:00 +0000

    renames and dirs changed

--- bmr/tests/test_bmr_equations.py
+++ bmr/tests/test_bmr_equations.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 from helpers.weight import WeightCalculator
-from helpers.basal_metabolic_rate import HarrisBenedictEqs
+from bmr.helpers.equations import HarrisBenedictEqs
 
 class TestBasalMetabolicRate(unittest.TestCase):
 

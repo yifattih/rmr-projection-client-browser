91111e27a92e35587356b1f7051fb399406176eb
yifattih <yifattih@proton.me>
2024-11-06 20:22:43 +0000

    feat(container app dev): wsgi in src dir

--- app/src/wsgi.py
+++ app/src/wsgi.py
@@ -5,4 +5,4 @@ This is the main file that starts the flask server and serves the web app UI in 
 """
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
